01dda94d600994fe1ac85af7412961cb09ede313
SandeepRavindu <sandeepravindu112@gmail.com>
2026-05-30 14:45:56 +0530

    fix: enable PATCH support for RestTemplate

--- src/main/java/com/example/bff/config/RestTemplateConfig.java
+++ src/main/java/com/example/bff/config/RestTemplateConfig.java
@@ -5,11 +5,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+        return restTemplate;
     }
 }
